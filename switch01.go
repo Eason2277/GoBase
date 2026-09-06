3dc1c6f7733b55f69c0dfad04da2c42802fe2f56
eason <zx89992819@163.com>
2026-09-06 15:41:41 +0800

    feat: add switch statement example

--- switch01.go
+++ switch01.go
@@ -1,1 +1,20 @@
 package main
+
+import "fmt"
+
+func main() {
+	var status int
+	fmt.Println("请输入状态码")
+	fmt.Scanln(&status)
+
+	switch status {
+	case 1:
+		fmt.Println("有待支付")
+
+	case 2:
+		fmt.Println("待发货")
+
+	default:
+		fmt.Println("未知状态")
+	}
+}
